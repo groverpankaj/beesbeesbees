d51dc6d8e35236ec698ab4e00076cc6b1968fa43
Pankaj <pgrover2079@gmail.com>
2019-12-16 12:14:02 -0800

    Completed

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.color = 'yellow';
+	this.canFly = true;
+	this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+ForagerBee.prototype.forage = function(val) {
+	this.treasureChest.push(val);
+}
+
+ForagerBee.prototype.constructor = ForagerBee;
